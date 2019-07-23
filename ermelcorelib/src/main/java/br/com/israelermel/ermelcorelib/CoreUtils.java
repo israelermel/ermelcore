25d9c3a4f25217889e6b93c69acf4aaba5e8a55d
israelermel <israelermel@gmail.com>
2019-07-22 22:43:52 -0300

    configuracao inicial

---
+++ ermelcorelib/src/main/java/br/com/israelermel/ermelcorelib/CoreUtils.java
@@ -0,0 +1,13 @@
+package br.com.israelermel.ermelcorelib;
+
+public class CoreUtils {
+
+    public static int convert(String valor) {
+        return Integer.valueOf(valor);
+    }
+
+    public static String convertString(int valor) {
+        return String.valueOf(valor);
+    }
+
+}
